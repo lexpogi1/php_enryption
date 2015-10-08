98b36b541256f9670b9a1d8e4aa22c269311c5c0
lexpogi1 <fatimareginabenitez@gmail.com>
2015-10-08 21:32:21 +0800

    Create loginform.php

---
+++ loginform.php
@@ -0,0 +1,25 @@
+<html>	
+<head>
+	<link rel="stylesheet" type="text/css" href="tableStyle.css">
+</head>
+<body>
+
+<form action="main.php" method="post">
+  <table bgcolor="#DEB887">
+  <tr>
+	<td>Username</td>
+	<td><input type="text" name="uname"></td>
+  </tr>
+  <tr>  
+     <td>Password</td>  
+     <td><input type="text" name="pass"></td>
+   </tr>
+   <tr>
+	<td colspan="2"><input type="submit"></td>
+  </tr>
+  
+  </table>
+</form> 
+
+</body>
+</html>
